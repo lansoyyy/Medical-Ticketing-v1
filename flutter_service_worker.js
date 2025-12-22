199838d59f3b1b0660d486e9f97b4b4c5642502e
Lans12345 <olanalans12345@gmail.com>
2025-12-23 05:46:01 +0800

    update

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -26,7 +26,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "2868a1f94410591ae460270f0367da3f
 "canvaskit/skwasm.wasm": "39dd80367a4e71582d234948adc521c0",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "83d881c1dbb6d6bcd6b42e274605b69c",
-"flutter_bootstrap.js": "88bbabfade80d39ed4a1bed5fe9cc652",
+"flutter_bootstrap.js": "5c1f9f93b63f7c9a393c57023264bf7e",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
